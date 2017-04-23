550498d5251b5567d661ae6cd66f6d0920f749cb
sixcorners <sixcorners@gmail.com>
2017-04-23 05:37:45 -0500

    Use javax.ws.rs.ApplicationPath to activate jaxrs.

---
+++ src/main/java/org/jboss/errai/demo/JaxrsActivator.java
@@ -0,0 +1,8 @@
+package org.jboss.errai.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class JaxrsActivator extends Application {
+}
